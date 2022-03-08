d3b89c3ea9b8b1ed11b1cefdedd0acc68ddf7034
RATSIMBAZAFY Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-03-08 14:35:56 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,226 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Floix - Onepage Resume Bootstrap Template</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<!-- Header Section -->
+<section class="banner" role="banner"> 
+  <!-- Navigation Section -->
+  <header id="header">
+    <div class="header-content"><a href="#"><img class="hei_logo" src="images/HEI.png" alt=""></a>
+    
+      <nav class="navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#introduction">Acceuil</a></li>
+          <li><a href="#formation">Formation</a></li><!--works formation se souvenir-->
+          <li><a href="https://bit.ly/3Kk4X2a">Concours</a></li> <!--à créer-->
+          <li><a href="#contact">Contacts</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+  </header>
+  <!-- Navigation Section --> 
+  <!-- Banner Section -->
+  <div class="container">
+    <div class="col-md-7 banner-inner-wrapper">
+      <div class="banner-text">
+        <h1>Bienvenue à l'Haute Ecole d'Informatique</h1>
+        <p>Professionnels du monde entier unis pour former les étudiants malgaches suivant les plus hauts standards.</p>
+        <a href="#formation" class="btn first-btn">En savoir plus</a> </div>
+      <!-- banner text --> 
+    </div>
+  </div>
+  <!-- Banner Section --> 
+</section>
+<!-- Header Section --> 
+<!-- Intro Section -->
+<section id="introduction" class="section introduction jumbotron">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="intro-content">
+          <h1>HEI : une école spécialisé en informatique</h1>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-6">
+        <div class="intro-content">
+          <p>A l'heure où tout se digitalise, des professionels malgaches du monde entier se sont réunis pour créer une école spécialisée dans l'informatique.</p>
+          <p>Fondée par le docteur <b>Lou Maurica</b> et <b>Ryan ANDRIAMAHERY</b> en 2021, HEI a pour ambition de former des étudiants performants dans le monde professionels.</p>
+          <p>Intéressé(e) ? N'attendez-pas!</p>
+          <a href="https://bit.ly/3Kk4X2a" class="btn ">Inscrivez-vous</a>
+        </div>
+      </div>
+      <div class="offset-md-1 col-md-2 col-sm-6">
+        <div class="intro-content">
+          <h4>Nos Valeurs</h4>
+          <ul>
+            <li> - Entraide</li>
+            <li> - Performance</li>
+            <li> - Discipline</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Intro Section --> 
+
+<!-- formation section -->
+<section id="formation" class="formation section no-padding">
+  <div class="container-fluid">
+    <h2>Notre équipe pédagogique</h2><br>
+    <div class="row justify-content-center">
+      <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/toky.jpg" class="formation-box"> <img src="images/toky.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <h4>Doctorant en Système d'Information Géomatique et Décision, Antananarivo</h4>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+        <h3>Tokimahery RAMAROZAKA</h3>
+       </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/sandrine.jpg" class="formation-box"> <img src="images/sandrine.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <h4>Ex-directrice technique, Vanilla Pay, Antananarivo | Révélation Fintech de Women In Africa (2020), Antananarivo</h4>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+        <h3>Sandrine RAMAROSON</h3>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/tahina.jpg" class="formation-box"> <img src="images/tahina.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <h4>Docteure-Ingénieure, CEA, Saclay | Prix L'Oréal-UNESCO pour les Femmes en Science (2017)</h4>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+        <h3>Tahina RALITERA</h3>
+       </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/Bako_Harry.jpg" class="formation-box"> <img src="images/Bako_Harry.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <h4>Ingénieur en Sécurité Informatique, Antananarivo</h4>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+        <h3>Bako Harry RAKOTONDRATOMPO</h3>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/Zo_Lalaina.jpg" class="formation-box"> <img src="images/Zo_Lalaina.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <h4>ML Tech Lead, Rocket Science, Ottawa</h4>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a>
+        <h3>Yannick RAHARIJAONA</h3>
+       </div>
+        <div class="col-lg-2 col-md-4 col-sm-6 formation sub_title"> <a href="images/parison.jpg" class="formation-box"> <img src="images/parison.jpg" alt="">
+          <div class="overlay">
+            <div class="overlay-caption">
+              <h4>Ingénieur logiciel, Google, Londres</h4>
+            </div>
+          </div>
+          <!-- overlay --> 
+          </a>
+          <h3>Parison RAVALOMANDA</h3>
+         </div>
+    </div>
+  </div><br>
+  <!-- Parcours -->
+  <div class="container-fluid  jumbotron sides">
+    <div class="row justify-content-center">
+    <div class="col-lg-6 col-md-6 col-sm-12">
+        <h2>Parcours</h2><br>
+        <h6>Nous proposons deux parcours jusqu’à la Licence : <b>Écosystèmes Logiciels</b> (formant des développeurs backend et frontend, développeurs mobile, analystes de données, chefs de projet…) et <b>Transformations Numériques</b> (formant des référenceurs web, commerçant-marketeurs digital, analystes de données, chefs de projet).</h6>
+        <br>
+        <a href="https://bit.ly/3tnGMJU" class="btn ">Notre Programme</a>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 justify-content-center"><img class="image_sides" src="images/console.jpg" alt="image missing"></div>
+    </div>
+    </div>
+    <!-- Bourses -->
+  <div class="container-fluid sides">
+    <div class="row-reverse justify-content-center">
+    <div class="col-lg-6 col-md-6 col-sm-12">
+        <h2>Bourses d'études</h2><br>
+        <h6>Les 3 étudiants les mieux classés au concours d’entrée se verront exonérés des frais de scolarité. Il en va de même pour 2 étudiants supplémentaires évalués sur critères sociaux.</h6><br>
+
+          <h6>Par ailleurs, HEI assure le financement à remboursement du Master en Informatique en France de l’étudiant(e) le plus méritant(e) à hauteur de 8.900€ grâce au programme Vatsy.</h6>        
+        <a href="https://bit.ly/3tnGMJU" class="btn ">Notre Programme</a>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 justify-content-center"><img class="image_sides" src="images/bourses.jpg" alt="image missing"></div>
+    </div>
+    </div>
+    <!-- Alliance -->
+  <div class="container-fluid jumbotron sides">
+    <div class="col-lg-6 col-md-6 col-sm-12">
+      <h2>Alliance Française</h2><br>
+      <h6>Chez HEI, la communication est aussi importante que la technique ! Les cours de français sont assurés par l’Alliance Française de Madagascar.</h6>
+      <a href="http://www.alliancefr.mg/" class="btn ">Alliance Française</a>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-6 col-sm-12 justify-content-center"><img class="image_sides" src="images/alliance.jpg" alt="image missing"></div>
+      </div>
+   
+    </div>  
+  
+    
+  </div>
+</section>
+<!-- formation section --> 
+<!-- footer section -->
+<footer id="contact" class="footer">
+  <div class="container">
+    <div class="col-md-4">
+      <h4>Contact</h4>
+      <p>
+        Adress: II J 161 R Ambodivoanjo Ivandry Antananarivo, 101 <br>
+        Call: +261 34 94 041 16 <br>
+        Email: <a href="contact@hei.school"> contact@hei.school </a></p>
+    </div>
+    <div class="col-md-3">
+      <h4>Social presense</h4>
+      <ul class="footer-share">
+        <li><a href="https://web.facebook.com/hei.school/?_rdc=1&_rdr"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="https://www.linkedin.com/company/hei-school/?originalSubdomain=mg"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+    <div class="col-md-5">
+      <p>© 2022 All rights reserved.<br>
+        Made with <i class="fa fa-heart pulse"></i> by <a href="http://www.designstub.com/">a student</a></p>
+    </div>
+  </div>
+</footer>
+<!-- footer section --> 
+
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/jquery.fancybox.pack.js"></script> 
+<script src="js/retina.min.js"></script> 
+<script src="js/modernizr.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
